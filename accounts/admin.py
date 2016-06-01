b670a99cdd1eb44d5a08d8f738873c2be88dad5a
hayashizakitakaaki <ground.fleet@gmail.com>
2016-06-01 13:38:12 +0900

    update accounts
    [fix]user table fix to custum user table

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,34 @@
 from django.contrib import admin
 import accounts
+from . import models
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
+
 
 # Register your models here.
 
 
-admin.site.register(accounts.models.User)
+class UserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = models.User
+
+
+class UserAdmin(UserAdmin):
+    form = UserChangeForm
+    fieldsets = (
+        (None, {'fields': ('username', 'password', 'screenname', )}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'password1', 'password2'),
+        }),
+    )
+    list_display = ('username', 'screenname', )
+    list_filter = ('username', )
+    search_fields = ('username', )
+    ordering = ('username', )
+    filter_horizontal = ()
+
+
+admin.site.register(models.User, UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,43 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 
 # Create your models here.
 
 
-class User(AbstractUser):
-    """ユーザー"""
-    group_name = models.CharField('所属', max_length=255, blank=True)
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, password, is_superuser, **extra_fields):
+        """
+        Creates and saves a User with the given username, email and password.
+        """
+        if not username:
+            raise ValueError('The given username must be set')
+        user = self.model(username=username, is_superuser=is_superuser, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, password, **extra_fields):
+        return self._create_user(username, password, True, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField('ユーザー名', max_length=30, unique=True)
+    screenname = models.CharField('ユーザー名（表示用）', max_length=255)
+    is_active = models.BooleanField('有効フラグ', default=True)
+    is_staff = models.BooleanField('スタッフ', default=True)
+    created_date = models.DateTimeField('登録日時', auto_now_add=True)
+    modified_date = models.DateTimeField('更新日時', auto_now=True)
+
+    objects = UserManager()
+    USERNAME_FIELD = 'username'
+
+    class Meta:
+        verbose_name = 'ユーザー'
+        verbose_name_plural = verbose_name
+
+    def get_full_name(self):
+        return self.__str__()
+
+    get_short_name = get_full_name
