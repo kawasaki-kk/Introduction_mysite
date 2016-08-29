b1a82ae38f960b44a3ab58c0dd29d85851c1bb8c
hayashizakitakaaki <ground.fleet@gmail.com>
2016-08-29 13:46:35 +0900

    [fix]del screen name and add first and last name

--- accounts/models.py
+++ accounts/models.py
@@ -28,8 +28,10 @@ class User(AbstractBaseUser, PermissionsMixin):
     # idは自動生成されるものを使用
     username = models.CharField('ユーザー名', max_length=30, unique=True,
                                 help_texxt="This using user ID and use login or logout")
-    screenname = models.CharField('ユーザー名（表示用）', max_length=255,
+    first_name = models.CharField('姓', max_length=255,
                                   help_text="")
+    last_name = models.CharField('名', max_length=255,
+                                 help_text="")
     is_active = models.BooleanField('有効フラグ', default=True)
     is_staff = models.BooleanField('スタッフ', default=True)
     created_date = models.DateTimeField('登録日時', auto_now_add=True)
@@ -44,6 +46,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     # 必須メソッドを定義
     def get_full_name(self):
-        return self.__str__()
+        return self.first_name + self.last_name
 
-    get_short_name = get_full_name
+    def get_short_name(self):
+        return self.first_name
