7a5d75abd363788984a12e43a9b98127db4b5deb
hayashizakitakaaki <ground.fleet@gmail.com>
2016-09-12 14:41:31 +0900

    [new]create dummy password file

---
+++ Introduction_mysite/ex_password.dummy.py
@@ -0,0 +1,11 @@
+u"""機密情報
+    データベース、及びdjangoシークレットキー情報の管理
+    ''内部にご自身で設定した情報をご記入ください
+    ご使用の際には、このファイルの".dummy"部分を削除してください
+    
+    settings.py内ですでにimportされているため、上記編集を行わない場合、正常に動作しません
+"""
+database_password = 'password here'
+database_user = 'your database user name here'
+database_name = 'your database name here'
+secret_key = 'django secret key here'
