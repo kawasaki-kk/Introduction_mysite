850d85b0f03afb579bd7c235988e7216aa811c3b
Koki Kawasaki <kawasaki.koki@tis.co.jp>
2016-09-20 14:04:57 +0900

    Qiita process added.

---
+++ dailyreport/qiita/yuba_analyze.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+from jubatus.recommender import client
+from jubatus.recommender import types
+from jubatus.common import Datum
+from .parser import load_json
+from .mecab import get_AllNouns
+
+NAME = "recommender_qiita";
+
+
+def recommend_Qiita(title, content, recommend_num = 4):
+	recommender = client.Recommender("127.0.0.1", 9199, NAME)
+	recommender.load('1018')
+
+	d = Datum(get_AllNouns(content))
+	recommender.update_row(title, d)
+	sr = recommender.similar_row_from_id(title , recommend_num)
+	print(sr)
+
+	data = []
+	for i in range(1, len(sr)):
+		print("i:",i)
+		temp = recommender.decode_row(sr[i].id).string_values
+		print(temp)
+		data.append({"id":sr[i].id, "score":sr[i].score, "url":_get_url_or_tags(temp, "url"), "tags":_get_url_or_tags(temp, "tags").split(",")})
+	return data
+
+def _get_url_or_tags(data_list, name):
+	if data_list:
+		return 	list(filter(lambda taple: taple[0]==name, data_list))[0][1]
+	else:
+		return []
+
+if __name__ == '__main__':
+	from pprint import pprint
+	recommender = client.Recommender("127.0.0.1", 9199, NAME)
+	recommender.load('1018')
+
+	with open('./data/nippo/20161013.txt') as f:
+		nippo = f.read().split('\n')
+
+	sr = recommend_Qiita(nippo[0], nippo[1])
+	print(sr)
+
+	print ("nippo ", nippo[0],  " is similar to :")
+	for data in sr:
+		pprint([data["id"], data["score"], data["url"], data["tags"]] )
+
+	#datam = recommender.decode_row(sr[1].id)
+	#print(datam.string_values[0][1])
+	#print(sr[1].id, recommender.decode_row(sr[1].id))
---
+++ dailyreport/qiita/yuba_update.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from jubatus.recommender import client
+from jubatus.recommender import types
+from jubatus.common import Datum
+
+from parser import load_json, get_all_files
+from mecab import get_AllNouns
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 9199
+NAME = "recommender_qiita"
+DATA_FILE_DIR = "./data/items/"
+
+if __name__ == '__main__':
+	recommender = client.Recommender(SERVER_IP, SERVER_PORT, NAME)
+
+	for file_name in get_all_files(DATA_FILE_DIR):
+		data = load_json(DATA_FILE_DIR, file_name)
+		print(data["title"])
+		d = Datum(
+			get_AllNouns(data["body"])
+			)
+
+		recommender.update_row(file_name, d)
