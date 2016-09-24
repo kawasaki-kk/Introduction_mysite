c054e0172c9648d1f8a31bdd280bad30cd338cdf
Koki Kawasaki <kawasaki.koki@tis.co.jp>
2016-09-24 13:59:23 +0900

    yuba_analyze modified change datum id, from article title to file name.

--- dailyreport/qiita/yuba_analyze.py
+++ dailyreport/qiita/yuba_analyze.py
@@ -2,33 +2,35 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
 from jubatus.recommender import client
 from jubatus.recommender import types
 from jubatus.common import Datum
-from services import load_json
-from mecab import get_AllNouns
+
+if __name__ == '__main__':
+	from services import load_json
+	from mecab import get_AllNouns
+else:
+	from dailyreport.qiita.services import load_json
+	from dailyreport.qiita.mecab import get_AllNouns
 
 NAME = "recommender_qiita";
 
 
 def recommend_Qiita(title, content, recommend_num = 4):
 	recommender = client.Recommender("127.0.0.1", 9199, NAME)
-	recommender.load('1018')
+	#recommender.load('1018')
 
 	d = Datum(get_AllNouns(content))
-	recommender.update_row(title, d)
-	similars = recommender.similar_row_from_id(title , recommend_num)
+	similars = recommender.similar_row_from_datum(d , recommend_num)
 	print(similars)
 
 	data = []
-	for similar in similars[1:]:
-		print("i:",similar)
-		#temp = recommender.decode_row(sr[i].id).string_values
-		#print(temp)
-		item = load_json("./data/item", similar.id)
+	for similar in similars:
+		print("similar:",similar)
+		item = load_json(os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/items"), similar.id)
 		#data.append({"id":sr[i].id, "score":sr[i].score, "url":_get_url_or_tags(temp, "url"), "tags":_get_url_or_tags(temp, "tags").split(",")})
-		data.append({"id":item["title"], "score":similar[i].score, "url":item["url"], "tags":item["tags"]})
-	print(data)
+		data.append({"title":item["title"], "score":similar.score, "url":item["url"], "tags":item["tags"]})
 	return data
 
 def _get_url_or_tags(data_list, name):
@@ -40,18 +42,14 @@ def _get_url_or_tags(data_list, name):
 if __name__ == '__main__':
 	from pprint import pprint
 	recommender = client.Recommender("127.0.0.1", 9199, NAME)
-	recommender.load('1018')
 
 	with open('./data/nippo/20161013.txt') as f:
 		nippo = f.read().split('\n')
 
 	sr = recommend_Qiita(nippo[0], nippo[1])
-	print(sr)
+	#print(sr)
 
 	print ("nippo ", nippo[0],  " is similar to :")
 	for data in sr:
-		pprint([data["id"], data["score"], data["url"], data["tags"]] )
+		pprint([data["title"], data["score"], data["url"], data["tags"]] )
 
-	#datam = recommender.decode_row(sr[1].id)
-	#print(datam.string_values[0][1])
-	#print(sr[1].id, recommender.decode_row(sr[1].id))
