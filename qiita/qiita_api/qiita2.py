6bd95c8c7b502f066cb87850dabe3689ce2f4eed
Koki Kawasaki <kawasaki.koki@tis.co.jp>
2016-11-04 17:38:01 +0900

    delete qiita1.py. qiita2.py modify can get all Qiita-articles. may be. by handling the date of last got article.

--- qiita/qiita_api/qiita2.py
+++ qiita/qiita_api/qiita2.py
@@ -3,12 +3,13 @@ import os
 
 import codecs
 import json
-from logging import getLogger
+from logging import getLogger, FileHandler
 import requests
 from urllib.parse import urlparse, parse_qs
-from services import load_json
 
 logger = getLogger(__name__)
+fh = FileHandler('test.log', 'a+')
+logger.addHandler(fh)
 
 URL_ITEMS     = "https://qiita.com/api/v2/items"
 URL_TAG_ITEMS = "https://qiita.com/api/v2/tags/%s/items"
@@ -20,14 +21,16 @@ LINK_LAST = "last"
 
 default_per_page = 100
 default_max_page = 100 
-wait_seconds = 12
-retry_wait_min = 1
-retry_limit = 10
+default_query = ""
+wait_seconds = 24
+retry_wait_min = 5 
+retry_limit = 2000
 
-auth_token = load_json(os.path.dirname(os.path.abspath(__file__)), "auth_token.json")["qiita"]
+with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), "auth_info.json")) as f:
+    auth_token = json.load(f)["access_token"]
 
-def items(per_page = default_per_page, max_page = default_max_page):
-    req = QiitaRequest(URL_ITEMS, per_page, max_page)
+def items(per_page = default_per_page, max_page = default_max_page, query = default_query):
+    req = QiitaRequest(URL_ITEMS, per_page, max_page, query)
     return QiitaIterator(req)
 
 def tag_items(tag_url, per_page = default_per_page, max_page = default_max_page):
@@ -67,17 +70,18 @@ class QiitaRequest:
 
     retry_num = 0
 
-    def __init__(self, url, per_page = default_per_page, max_page = default_max_page, page = 1):
+    def __init__(self, url, per_page = default_per_page, max_page = default_max_page, query = default_query, page = 1):
         self.url = url
         self.per_page = per_page
         self.max_page = max_page
+        self.query = query
         self.page = page
         self.res = None
         self.current_page = None
 
     def request(self):
         self.links = dict()
-        params = {"per_page": self.per_page, "page": self.page}
+        params = {"per_page": self.per_page, "page": self.page, "query":self.query}
         return self.__request__(self.url, params)
 
     def __request__(self, url, params = None):
@@ -85,7 +89,7 @@ class QiitaRequest:
         logger.info("url:%s" % url)
 
         headers = {"Authorization": "Bearer " + auth_token} if auth_token != None else None
-        self.res = requests.get(url, params = params, headers = headers)
+        self.res = requests.get(url, params = params, headers = headers, )
         status = self.res.status_code
 
         while status != 200 and QiitaRequest.retry_num <= retry_limit:
@@ -143,14 +147,26 @@ class QiitaRequest:
 
 def save_item(item):
     item_id = item["id"]
-    filename = "data/items/%s.json" % item_id
+    filename = "../data/items/%s.json" % item_id
     with codecs.open(filename, "w", "utf-8") as f:
         f.write(json.dumps(item, indent = 4, ensure_ascii=False))
 
 if __name__ == '__main__':
-    max_page_num = len(items())//100 + 1
-    print("max_page_num is {}".format(max_page_num))
+    from dateutil.parser import parse
+    from datetime import date, datetime, timedelta  
 
-    for i in items():
-        print(i["title"])
-        save_item(i)
+    # total_page = len(items())
+    total_page = 200000
+    total_trial_num = total_page//default_max_page + 1
+    last_day = date.today().strftime("%Y-%m-%d")
+    print("total_page", total_page)
+    print("tatal_trial_num", total_trial_num)
+    print("last_day", last_day)
+
+    for trial_num in range(total_trial_num): 
+        print("Trial_num:{}".format(trial_num))
+        for item in items(query="created:<"+last_day):
+            print(item["title"], end=' / ')
+            save_item(item)
+        last_day = (parse(item["created_at"])-timedelta(days=1)).strftime("%Y-%m-%d")
+        print("Last_day:{}".format(last_day))
